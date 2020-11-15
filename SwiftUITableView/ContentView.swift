b7d03cb417d607e3dc7947f98d790e7ef01b91a9
Christopher Hull <cpahull@btinternet.com>
2020-11-15 15:07:26 +0000

    Initial commit. Table updates and clears based on State variable. Next trick is to pass selectedRow back into Content.Swift ...

--- SwiftUITableView/ContentView.swift
+++ SwiftUITableView/ContentView.swift
@@ -5,15 +5,53 @@
 //  Created by cpahull on 14/11/2020.
 //
 
+import Foundation
 import SwiftUI
+import AppKit
 
 struct ContentView: View {
+    
+    @State private var myNames = [[String]]()
+
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                Button("Refresh") {
+                    myNames = [
+                        ["Chris", "Hull"],
+                        ["Lynn", "Harris"],
+                        ["Kirsty", "Chambers"],
+                        ["Caroline", "Whatman"]
+                        ]
+                }
+                Button("Clear") {
+                    myNames.removeAll()
+                }
+            }
+            TableView(myNames: $myNames)
+                .frame(width: 450, height: 300)
+        }
+        
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
+struct TableView: NSViewControllerRepresentable {
+    
+    @Binding var myNames: [[String]]
+    
+    typealias NSViewControllerType = TableViewController
+    
+    func makeNSViewController(context: NSViewControllerRepresentableContext<TableView>) -> TableViewController {
+        return TableViewController()
+    }
+    
+    func updateNSViewController(_ nsViewController: TableViewController, context: NSViewControllerRepresentableContext<TableView>) {
+        nsViewController.getNames(myNames: myNames)
+        return
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- SwiftUITableView/TableViewController.swift
+++ SwiftUITableView/TableViewController.swift
@@ -2,40 +2,43 @@
 //  TableViewController.swift
 //  SwiftUITableView
 //
-//  Created by cpahull on 14/11/2020.
+//  Created by cpahull on 15/11/2020.
 //
 
 import Cocoa
-
-var names: [[String]] = [
-                        ["Chris", "Hull"],
-                        ["Lynn", "Harris"],
-                        ["Kirsty", "Chambers"],
-                        ["Caroline", "Whatman"]
-                        ]
+import SwiftUI
 
 class TableViewController: NSViewController {
     
+    var names: [[String]]?
+
     @IBOutlet weak var tableView: NSTableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.delegate = self as NSTableViewDelegate
+        self.tableView.delegate = self
         self.tableView.dataSource = self
         // Do view setup here.
     }
     
 }
 
-extension TableViewController:NSTableViewDataSource, NSTableViewDelegate{
+extension TableViewController: NSTableViewDataSource, NSTableViewDelegate {
+    
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return names.count
+        return names?.count ?? 0
     }
+    
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         var result: NSTableCellView
         result = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as! NSTableCellView
-        result.textField?.stringValue = names[row][Int((tableColumn?.identifier.rawValue)!)!]
+        result.textField?.stringValue = names?[row][Int((tableColumn?.identifier.rawValue)!)!] ?? "None"
         return result
+    }
+    
+    func getNames(myNames: [[String]]) -> Void {
+        names = myNames
+        tableView.reloadData()
     }
 }
 
